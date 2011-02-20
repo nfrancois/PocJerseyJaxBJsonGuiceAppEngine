a25597751ac2dd2ae39ab1eda0d30d92b243ced5
nfrancois <nicolas.franc@gmail.com>
2011-02-20 13:32:50 +0000

    y a du mieux
    git-svn-id: http://nicolasfrancois.googlecode.com/svn/trunk/PocJerseyJaxbJsonGuiceAppEngine@56 0c6e3020-dc5f-4b39-ec15-eab0643b8c4e

--- src/test/java/nfrancois/poc/jerseyjaxbjsonguiceappengine/resource/DoubleHelloResourceTest.java
+++ src/test/java/nfrancois/poc/jerseyjaxbjsonguiceappengine/resource/DoubleHelloResourceTest.java
@@ -2,7 +2,9 @@ package nfrancois.poc.jerseyjaxbjsonguiceappengine.resource;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.core.MediaType;
 
@@ -11,25 +13,50 @@ import nfrancois.poc.jerseyjaxbjsonguiceappengine.model.Hello;
 
 import org.junit.Test;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceFilter;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.ClientResponse.Status;
 import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
 import com.sun.jersey.test.framework.JerseyTest;
 import com.sun.jersey.test.framework.WebAppDescriptor;
 
 public class DoubleHelloResourceTest extends JerseyTest {
 	
+//	private static Injector injector =  Guice.createInjector(new ServletModule() {
+//		
+//		@Override
+//		protected void configureServlets() {
+//			final Map<String, String> params = new HashMap<String, String>();
+//			bind(DoubleHelloResource.class);
+//			serve("/*").with(GuiceContainer.class, params);
+//		}
+//	});	
+	
     public DoubleHelloResourceTest() {
-        super(new WebAppDescriptor.Builder("nfrancois.poc.jerseyjaxbjsonguiceappengine.resource.JAXBContextResolver")
+        super(new WebAppDescriptor.Builder(/*JAXBContextResolver.class.getPackage().getName()*/)
                 .contextListenerClass(GuiceServletConfig.class)
                 .filterClass(GuiceFilter.class)
+                .clientConfig(createClientConfigTest())
                 .servletPath("/")
                 .build());
     }
 	
 	
+	private static ClientConfig createClientConfigTest() {
+		ClientConfig clientConfig = new DefaultClientConfig();
+		clientConfig.getClasses().add(JAXBContextResolver.class);
+		return clientConfig;
+	}
+
+
 	@Test
 	public void shoudHaveTwoHelloInNormalJson(){
 		String relativeUrl = "doublehello";
@@ -40,8 +67,8 @@ public class DoubleHelloResourceTest extends JerseyTest {
 		int status = clientResponse.getStatus();
 		assertThat(clientResponse.getType()).isEqualTo(MediaType.APPLICATION_JSON_TYPE);
 		assertThat(status).isEqualTo(Status.OK.getStatusCode());
-		List<Hello> responseAsList = path.type(MediaType.APPLICATION_JSON_TYPE).get(new GenericType<List<Hello>>(){});
-//		assertThat(responseAsList).isNotNull().hasSize(2);
+		List<Hello> responseAsList = path.get(new GenericType<List<Hello>>(){});
+		assertThat(responseAsList).isNotNull().hasSize(2);
 		String reponseAsString = path.get(String.class);
 		assertThat(reponseAsString).isEqualTo("[{\"message\":\"Hello\",\"name\":\"Nicolas\"},{\"message\":\"Hello\",\"name\":\"Nicolas\"}]");
 	}
@@ -50,4 +77,11 @@ public class DoubleHelloResourceTest extends JerseyTest {
 		return getBaseURI().toString()+relativeUrl+"/"+name;
 	}	
 	
+//	
+//	public class GuiceTestConfig extends GuiceServletContextListener {
+//		@Override
+//		public Injector getInjector() {
+//			return injector;
+//		}
+//	}		
 }
