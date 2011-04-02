df63b64beb84b52b6fcc0e86107aaddb9ac9a4e8
nfrancois <nicolas.franc@gmail.com>
2011-04-02 23:56:08 +0000

    Mise à jour version GAE/Guice/Guava
    git-svn-id: http://nicolasfrancois.googlecode.com/svn/trunk/PocJerseyJaxbJsonGuiceAppEngine@95 0c6e3020-dc5f-4b39-ec15-eab0643b8c4e

--- src/main/java/nfrancois/poc/jerseyjaxbjsonguiceappengine/model/Hello.java
+++ src/main/java/nfrancois/poc/jerseyjaxbjsonguiceappengine/model/Hello.java
@@ -4,6 +4,8 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.google.common.base.Objects;
+
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Hello {
@@ -33,32 +35,17 @@ public class Hello {
 	}
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((message == null) ? 0 : message.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
+		return Objects.hashCode(message, name);
 	}
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Hello other = (Hello) obj;
-		if (message == null) {
-			if (other.message != null)
-				return false;
-		} else if (!message.equals(other.message))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
+	    if(obj instanceof Hello){
+	        final Hello other = (Hello) obj;
+	        return Objects.equal(message, other.message)
+	            && Objects.equal(name, other.name);
+	    } else{
+	        return false;
+	    }
 	}
 
 }
